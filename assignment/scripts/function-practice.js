0d5c3f740aa2ce8a7194f73c7ee744539fa3e2b2
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-04-19 11:53:41 -0500

    added console.logs to show different questions in console.

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -4,6 +4,7 @@ console.log('***** Function Practice *****')
 // After _each_ function, use a console log to call the function
 // to test it and display the result
 
+console.log('----- Q1 -----');
 // 1. Function to return 'Hello World!'
 function hello() {
   return 'Hello World!';
@@ -15,6 +16,7 @@ console.log('Test - should say "Hello World!"', hello());
 
 
 
+console.log('----- Q2 -----');
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
@@ -28,6 +30,7 @@ console.log(helloName('Isaac'));
 
 
 
+console.log('----- Q3 -----');
 // 3. Function to add two numbers together & return the result
 function addNumbers( firstNumber, secondNumber ) {
 
@@ -41,6 +44,7 @@ console.log(addNumbers(1,3));
 
 
 
+console.log('----- Q4 -----');
 // 4. Function to multiply three numbers & return the result
 function multiplyThree( firstNumber, secondNumber, thridNumber ) {
 
@@ -53,6 +57,7 @@ console.log(multiplyThree(3,4,5));
 
 
 
+console.log('----- Q5 -----');
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
@@ -73,6 +78,7 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 
 
 
+console.log('----- Q6 -----');
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast( array ) {
@@ -92,6 +98,7 @@ console.log(getLast());
 
 
 
+console.log('----- Q8 -----');
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
@@ -109,6 +116,7 @@ console.log(find());
 // ----------------------
 // Stretch Goals
 // ----------------------
+console.log('----- Q8 -----');
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
@@ -117,6 +125,7 @@ function isFirstLetter(letter, string) {
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
+console.log('----- Q9 -----');
 // 9. Function to return the sum of all numbers in an array
 function sumAll( ) {
   let sum = 0
@@ -124,12 +133,14 @@ function sumAll( ) {
   return sum;
 }
 
+console.log('----- Q10 ----');
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 
 
 
+console.log('----- Q11 ----');
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
